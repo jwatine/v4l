c00bcaca2244a2fa8f9f5fca9ffe1ad3d05c4d9d
jwatine <watinejeanbaptiste@gmail.com>
2018-07-13 17:27:21 +0200

    lot of frames git add * ( still buggy)

--- two_cameras_YUV_format/open_device.c
+++ two_cameras_YUV_format/open_device.c
@@ -14,6 +14,11 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 
+
+#define WIDTH 640
+#define HEIGHT 480
+#define PIX 2
+
 #define MILLISEC 0.001
 #define NANO_TO_MICRO 1000
 //#define DEBUG
@@ -257,13 +262,13 @@ void mem_needed_device(int fd,  struct v4l2_buffer * bufferinfo){
 /* map the memory for the buffer */
 void allocate_mem(int fd,  struct v4l2_buffer * bufferinfo,void ** buffer_start){
   *buffer_start = mmap(
-		      NULL,
-		      bufferinfo->length,
-		      PROT_READ | PROT_WRITE,
-		      MAP_SHARED,
-		      fd,
-		      bufferinfo->m.offset
-		      );
+		       NULL,
+		       bufferinfo->length,
+		       PROT_READ | PROT_WRITE,
+		       MAP_SHARED,
+		       fd,
+		       bufferinfo->m.offset
+		       );
   
   if(*buffer_start == MAP_FAILED){
     perror("mmap");
@@ -289,9 +294,24 @@ void coeff_YUC_buffer_data( void ** buffer_start,struct v4l2_buffer * bufferinfo
   for (i=0; i<bufferinfo->length;i++){
     (*(unsigned char **)buffer_start)[i]+=coefficient;
   }
-
   coefficient++;
+
 }
+
+
+void  image_4_buffer_data(void * buffer_start, struct v4l2_buffer * bufferinfo,char * buffer_little_image,int div){
+  int i,j;
+  for(j=0;j<HEIGHT/2/div-1;j=j+1){
+    for (i=0; i<WIDTH*PIX/2/div;i=i+4){
+      buffer_little_image[i+j*WIDTH*PIX/2]=((unsigned char *)buffer_start)[i*2+j*WIDTH*PIX*2/div];
+      buffer_little_image[i+j*WIDTH*PIX/2+1]=((unsigned char *)buffer_start)[i*2+j*WIDTH*PIX*2/div+1];
+      buffer_little_image[i+j*WIDTH*PIX/2+2]=((unsigned char *)buffer_start)[i*2+j*WIDTH*PIX*2/div+2];
+      buffer_little_image[i+j*WIDTH*PIX/2+3]=((unsigned char *)buffer_start)[i*2+j*WIDTH*PIX*2/div+3];
+    }
+  }
+}
+
+
 
 void reverse_buffer_data(void ** buffer_start, struct v4l2_buffer * bufferinfo){
   
@@ -343,31 +363,78 @@ void stream(int fd1,  struct v4l2_format format1,void * buffer_start1,  struct v
 
   
   //create an YUV overlay and the rect that "contains" the frame
-  SDL_Overlay* frame1, *frame2,*frame3,*frame4;
+  SDL_Overlay* frame111,* frame112,* frame113,* frame114,* frame12,* frame13,* frame14, *frame2,*frame3,*frame4;
   
-  frame1 = SDL_CreateYUVOverlay(format1.fmt.pix.width,
-			       format1.fmt.pix.height,
-			       SDL_YUY2_OVERLAY,
-			       screen);
+  frame111 = SDL_CreateYUVOverlay(format1.fmt.pix.width/4,
+				  format1.fmt.pix.height/4,
+				  SDL_YUY2_OVERLAY,
+				  screen);
+  frame112= SDL_CreateYUVOverlay(format1.fmt.pix.width/4,
+				 format1.fmt.pix.height/4,
+				 SDL_YUY2_OVERLAY,
+				 screen);
+  frame113 = SDL_CreateYUVOverlay(format1.fmt.pix.width/4,
+				  format1.fmt.pix.height/4,
+				  SDL_YUY2_OVERLAY,
+				  screen);
+  frame114 = SDL_CreateYUVOverlay(format1.fmt.pix.width/4,
+				  format1.fmt.pix.height/4,
+				  SDL_YUY2_OVERLAY,
+				  screen);
+	
+  frame12 = SDL_CreateYUVOverlay(format1.fmt.pix.width/2,
+				 format1.fmt.pix.height/2,
+				 SDL_YUY2_OVERLAY,
+				 screen);
+  frame13 = SDL_CreateYUVOverlay(format1.fmt.pix.width/2,
+				 format1.fmt.pix.height/2,
+				 SDL_YUY2_OVERLAY,
+				 screen);
+  frame14 = SDL_CreateYUVOverlay(format1.fmt.pix.width/2,
+				 format1.fmt.pix.height/2,
+				 SDL_YUY2_OVERLAY,
+				 screen);
+  
+  
   frame2 = SDL_CreateYUVOverlay(format2.fmt.pix.width,
-			       format2.fmt.pix.height,
-			       SDL_YUY2_OVERLAY,
-			       screen);
+				format2.fmt.pix.height,
+				SDL_YUY2_OVERLAY,
+				screen);
   frame3 = SDL_CreateYUVOverlay(format1.fmt.pix.width,
-			       format1.fmt.pix.height,
-			       SDL_YUY2_OVERLAY,
-			       screen);
+				format1.fmt.pix.height,
+				SDL_YUY2_OVERLAY,
+				screen);
   frame4 = SDL_CreateYUVOverlay(format1.fmt.pix.width,
-			       format1.fmt.pix.height,
-			       SDL_YUY2_OVERLAY,
-			       screen);
+				format1.fmt.pix.height,
+				SDL_YUY2_OVERLAY,
+				screen);
 
-  SDL_Rect position1 = {.x = 0, .y = 0,.w = format1.fmt.pix.width , .h=format1.fmt.pix.height};
+  SDL_Rect position111 = {.x = 0, .y = 0,.w = format1.fmt.pix.width/4 , .h=format1.fmt.pix.height/4};
+  SDL_Rect position112 = {.x = 160, .y = 0,.w = format1.fmt.pix.width/4 , .h=format1.fmt.pix.height/4};
+  SDL_Rect position113 = {.x = 0, .y = 120,.w = format1.fmt.pix.width/4 , .h=format1.fmt.pix.height/4};
+  SDL_Rect position114 = {.x = 160, .y = 120,.w = format1.fmt.pix.width/4 , .h=format1.fmt.pix.height/4};
+
+ 
+
+  SDL_Rect position12 = {.x = 320, .y = 0,.w = format1.fmt.pix.width/2 , .h=format1.fmt.pix.height/2};
+  SDL_Rect position13 = {.x = 0, .y = 240,.w = format1.fmt.pix.width/2 , .h=format1.fmt.pix.height/2};
+  SDL_Rect position14 = {.x = 320, .y = 240,.w = format1.fmt.pix.width/2 , .h=format1.fmt.pix.height/2};
+  
   SDL_Rect position2 = {.x = 640, .y = 480,.w = format1.fmt.pix.width , .h=format2.fmt.pix.height};
   SDL_Rect position3 = {.x = 640, .y = 0,.w = format1.fmt.pix.width , .h=format1.fmt.pix.height};
   SDL_Rect position4 = {.x = 0, .y = 480,.w = format1.fmt.pix.width , .h=format2.fmt.pix.height};
 
+
   
+  char  buffer_little_image11[WIDTH*HEIGHT*PIX/16];
+  char  buffer_little_image12[WIDTH*HEIGHT*PIX/16];
+  char  buffer_little_image13[WIDTH*HEIGHT*PIX/16];
+  char  buffer_little_image14[WIDTH*HEIGHT*PIX/16];
+
+  char  buffer_little_image1[WIDTH*HEIGHT*PIX/4];
+  char  buffer_little_image2[WIDTH*HEIGHT*PIX/4];
+  char  buffer_little_image3[WIDTH*HEIGHT*PIX/4];
+  char  buffer_little_image4[WIDTH*HEIGHT*PIX/4];
   
   /* main loop */
   /* queue the buffer and dequeue it when it's filled */
@@ -407,34 +474,95 @@ void stream(int fd1,  struct v4l2_format format1,void * buffer_start1,  struct v
       reverse_buffer_data(&buffer_start2,bufferinfo2);
     }
 
+       
+    
+    image_4_buffer_data(buffer_start1, bufferinfo1, buffer_little_image1,1);
+    image_4_buffer_data(buffer_little_image1, bufferinfo1, buffer_little_image11,2);
+
+    image_4_buffer_data(buffer_start2, bufferinfo2, buffer_little_image2,1);
+    image_4_buffer_data(buffer_little_image2, bufferinfo1, buffer_little_image12,2);
+    
     //cpy the data from the device's buffers to the frame's buffers
-    memcpy(frame1->pixels[0],buffer_start1, bufferinfo1->length);
-    frame1->pitches[0] = 320;
+    memcpy(frame111->pixels[0],buffer_little_image11, bufferinfo2->length/16);
+    frame111->pitches[0] = 160;
+    memcpy(frame112->pixels[0],buffer_little_image12, bufferinfo2->length/16);
+    frame112->pitches[0] = 160;
+
+   
+
+    memcpy(frame12->pixels[0],buffer_little_image2, bufferinfo2->length/4);
+    frame12->pitches[0] = 160;
     memcpy(frame2->pixels[0],buffer_start2, bufferinfo2->length);
     frame2->pitches[0] = 320;
 
+
+    
     //some operation on buffers to get some psychedelic images ! 
     coeff_YUC_buffer_data(&buffer_start1,bufferinfo1);
     coeff_YUC_buffer_data(&buffer_start2,bufferinfo2);
 
+    
+    image_4_buffer_data(buffer_start1, bufferinfo1, buffer_little_image3,1);
+    image_4_buffer_data(buffer_little_image3, bufferinfo1, buffer_little_image13,2);
+    image_4_buffer_data(buffer_start2, bufferinfo1, buffer_little_image4,2);
+    image_4_buffer_data(buffer_little_image4, bufferinfo1, buffer_little_image14,2);
+       
     //cpy the operated data to the frame's buffers.
+    memcpy(frame13->pixels[0],buffer_little_image3, bufferinfo2->length/4);
+    frame13->pitches[0] = 160;
+
+    memcpy(frame113->pixels[0],buffer_little_image13, bufferinfo2->length/16);
+    frame113->pitches[0] = 80;
+    memcpy(frame114->pixels[0],buffer_little_image14, bufferinfo2->length/16);
+    frame114->pitches[0] = 80;
+
+    memcpy(frame14->pixels[0],buffer_little_image4, bufferinfo2->length/4);
+    frame14->pitches[0] = 160;
     memcpy(frame3->pixels[0],buffer_start1, bufferinfo1->length);
-    frame1->pitches[0] = 320;
+    frame3->pitches[0] = 320;
     memcpy(frame4->pixels[0],buffer_start2, bufferinfo2->length);
-    frame2->pitches[0] = 320;
+    frame4->pitches[0] = 320;
 
 
 
     /*** third task : display the video  ***/
     //lock the frame so that we can copy new values in the buffer data
-    SDL_LockYUVOverlay(frame1);
+    SDL_LockYUVOverlay(frame111);
+    SDL_LockYUVOverlay(frame112);
+    SDL_LockYUVOverlay(frame113);
+    SDL_LockYUVOverlay(frame114);
+
+    SDL_LockYUVOverlay(frame12);
+    SDL_LockYUVOverlay(frame13);
+    SDL_LockYUVOverlay(frame14);
     SDL_LockYUVOverlay(frame2);
     SDL_LockYUVOverlay(frame3);
     SDL_LockYUVOverlay(frame4);
  
     
     //display effectively the frame to the screen with right position
-    if(SDL_DisplayYUVOverlay(frame1,&position1)){
+    if(SDL_DisplayYUVOverlay(frame111,&position111)){
+      perror("DisplayYUVOverlay");
+    }
+    if(SDL_DisplayYUVOverlay(frame112,&position112)){
+      perror("DisplayYUVOverlay");
+    }
+
+    if(SDL_DisplayYUVOverlay(frame113,&position113)){
+      perror("DisplayYUVOverlay");
+    }
+
+    if(SDL_DisplayYUVOverlay(frame114,&position114)){
+      perror("DisplayYUVOverlay");
+    }
+  
+    if(SDL_DisplayYUVOverlay(frame12,&position14)){
+      perror("DisplayYUVOverlay");
+    }
+    if(SDL_DisplayYUVOverlay(frame13,&position13)){
+      perror("DisplayYUVOverlay");
+    }
+    if(SDL_DisplayYUVOverlay(frame14,&position12)){
       perror("DisplayYUVOverlay");
     }
     if(SDL_DisplayYUVOverlay(frame2,&position2)){
@@ -449,7 +577,14 @@ void stream(int fd1,  struct v4l2_format format1,void * buffer_start1,  struct v
 
 
     //unlock...
-    SDL_UnlockYUVOverlay(frame1);
+    SDL_UnlockYUVOverlay(frame111);
+    SDL_UnlockYUVOverlay(frame112);
+    SDL_UnlockYUVOverlay(frame113);
+    SDL_UnlockYUVOverlay(frame114);
+
+    SDL_UnlockYUVOverlay(frame12);
+    SDL_UnlockYUVOverlay(frame13);
+    SDL_UnlockYUVOverlay(frame14);
     SDL_UnlockYUVOverlay(frame2);
     SDL_UnlockYUVOverlay(frame3);
     SDL_UnlockYUVOverlay(frame4);
@@ -471,7 +606,16 @@ void stream(int fd1,  struct v4l2_format format1,void * buffer_start1,  struct v
   
 
   // Free everything, and unload SDL.
-  SDL_FreeYUVOverlay(frame1);
+  SDL_FreeYUVOverlay(frame111);
+  SDL_FreeYUVOverlay(frame112);
+  SDL_FreeYUVOverlay(frame113);
+  SDL_FreeYUVOverlay(frame114);
+
+  
+  SDL_FreeYUVOverlay(frame12);
+  SDL_FreeYUVOverlay(frame13);
+  SDL_FreeYUVOverlay(frame14);
+      
   SDL_FreeYUVOverlay(frame2);
   SDL_FreeYUVOverlay(frame3);
   SDL_FreeYUVOverlay(frame4);
@@ -502,8 +646,18 @@ int main(void){
 
   int fd1,fd2;
   /* open the devices */
-  open_fd(&fd1);
-  open_fd(&fd2);
+  /* open_fd(&fd1); */
+  /* open_fd(&fd2); */
+
+  if((fd1 = open("/dev/video0", O_RDWR)) < 0){
+    perror("open");
+    exit(1);
+  }
+  if((fd2 = open("/dev/video1", O_RDWR)) < 0){
+    perror("open");
+    exit(1);
+  }
+
   
 
   /* check for the device's capability */
